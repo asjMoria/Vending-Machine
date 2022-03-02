e3c365acdf3617d2f20ddea045e1417d7b61b184
a-r-t <alexthimineur@gmail.com>
2022-03-01 23:13:20 -0500

    created snack class

---
+++ src/Snack.java
@@ -0,0 +1,36 @@
+
+public class Snack {
+	private String name;
+	private double cost;
+	
+	public Snack(String name, double cost) {
+		super();
+		this.name = name;
+		this.cost = cost;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getCost() {
+		return cost;
+	}
+
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s: %.2f", name, cost);
+	}
+	
+	public static Snack of(String name, double cost) {
+		return new Snack(name, cost);
+	}
+}
